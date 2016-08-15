a5daa5edc0d15ace6bba99e698b423ddb6720c38
sindbad911 <sindbad911@bk.ru>
2016-08-16 00:28:19 +0300

    update conf

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-var app = require("./app.js");
-var http = require("http");
-var configs = require("./configs.js");
+/*!
+ * HB.js
+ * Copyright(c) 2016 HB.js Team
+ * MIT Licensed
+ */
 
+'use strict';
 
-var instance = http.createServer(app);
-
-instance.listen(configs.server.port);
-instance.on('error', function(error){
-    if(error.code == "EADDRINUSE"){
-        console.error('Port ' + configs.server.port + ' is already in use');
-        process.exit(1);
-    }else{
-        console.error(error);
-        process.exit(1);
-    }
-});
-
-instance.on('listening', function(){
-    console.log("Server running at 127.0.0.1:" + configs.server.port);
-    console.log(((configs.general.secure) ? "https://" : "http://") + configs.general.url);
-});
+var Server = require("./server");
+Server.start();
---
+++ server.js
@@ -0,0 +1,32 @@
+var app = require("./app.js");
+var http = require("http");
+var configs = require("./configs.js");
+
+
+var Server = function(){};
+
+Server.start = function(){
+    var instance = http.createServer(app);
+
+    instance.listen(configs.server.port);
+    instance.on('error', function(error){
+        if(error.code == "EADDRINUSE"){
+            console.error('Port ' + configs.server.port + ' is already in use');
+            process.exit(1);
+        }else{
+            console.error(error);
+            process.exit(1);
+        }
+    });
+
+    instance.on('listening', function(){
+        console.log("Server running at 127.0.0.1:" + configs.server.port);
+        console.log(((configs.general.secure) ? "https://" : "http://") + configs.general.url);
+    });
+};
+
+Server.stop = function(){
+    process.exit(1);
+};
+
+module.exports = Server;
