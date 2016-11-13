c086e306ff0fddb267881a4ed332d3f93e3f20f4
sindbad911 <sindbad911@bk.ru>
2016-11-13 02:17:31 +0200

    fix tests

--- test/integration/model.Users.js
+++ test/integration/model.Users.js
@@ -1,14 +1,87 @@
-var chai = require('chai'),
-    expect = chai.expect,
-    should = chai.should;
+var expect = require('chai').expect;
 var Users = require("../../app/services/Users");
 
 describe("Users CRUD", function(){
 
-    it("Creating user", function(done){
+    it("Creating user (Local auth)", function(done){
         Users.createUser("local", null, "tessst@bk.ru", null, null, "123456").then(function(res){
-            expect(res).to.be.true;
+            expect(res).to.exist;
+            expect(res).to.be.an('object');
+           done();
+        }, done);
+    });
+
+    it("Creating user (Twitter auth)", function(done){
+        Users.createUser("twitter", "1235454455454", null, "John", "Doe", null).then(function(res){
+            expect(res).to.exist;
+            expect(res).to.be.an('object');
+           done();
+        }, done);
+    });
+
+    it("Creating user (Google auth)", function(done){
+        Users.createUser("google", "1235454455454", null, "John", "Doe", null).then(function(res){
+            expect(res).to.exist;
+            expect(res).to.be.an('object');
+           done();
+        }, done);
+    });
+
+    it("Creating user (Facebook auth)", function(done){
+        Users.createUser("facebook", "1235454455454", null, "John", "Doe", null).then(function(res){
+            expect(res).to.exist;
+            expect(res).to.be.an('object');
+           done();
+        }, done);
+    });
+
+    it("Find user by email", function(done){
+        Users.findUserByEmail("tessst@bk.ru", "123456").then(function(res){
+            expect(res).to.exist;
+            expect(res).to.be.an('object');
             done();
         }, done);
     });
+
+    it("Find user by social id (Facebook)", function(done){
+        Users.findUserBySocialId("facebook", "1235454455454").then(function(res){
+            expect(res).to.exist;
+            expect(res).to.be.an('object');
+           done();
+        }, done);
+    });
+
+    it("Find user by social id (Google)", function(done){
+        Users.findUserBySocialId("google", "1235454455454").then(function(res){
+            expect(res).to.exist;
+            expect(res).to.be.an('object');
+           done();
+        }, done);
+    });
+
+    it("Find user by social id (Twitter)", function(done){
+        Users.findUserBySocialId("twitter", "1235454455454").then(function(res){
+            expect(res).to.exist;
+            expect(res).to.be.an('object');
+           done();
+        }, done);
+    });
+
+    it("Find used email (checking before auth)", function(done){
+        Users.findUsedEmail("tessst@bk.ru").then(function(res){
+            expect(res).to.exist;
+            expect(res).to.be.an('object');
+           done();
+        }, done);
+    });
+
+    it("Get user by ID", function(done){
+        Users.getUserById(1).then(function(res){
+            expect(res).to.exist;
+            expect(res).to.be.an('object');
+           done();
+        }, done);
+    });
+
+
 });
